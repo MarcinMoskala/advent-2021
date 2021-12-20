56a2f4ab74f9977855b39f6fd89e1470b0ec2ddb
marcinmoskala <marcinmoskala@gmail.com>
2021-12-20 19:16:17 +0100

    Add Week4

---
+++ src/main/kotlin/week4/JsonParse.kt
@@ -0,0 +1,5 @@
+package week4
+
+import week1.JsonElement
+
+fun parseJson(text: String): JsonElement? = TODO()
---
+++ src/test/kotlin/week4/JsonParseTest.kt
@@ -0,0 +1,148 @@
+package week4
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import week1.*
+
+class JsonParseTest {
+    @Test
+    fun `should stringify empty object`() {
+        assertEquals(JsonObject(), parseJson("""{}"""))
+    }
+
+    @Test
+    fun `should stringify null`() {
+        assertEquals(JsonObject("n" to JsonNull), parseJson("""{"n":null}"""))
+    }
+
+    @Test
+    fun `should stringify number`() {
+        assertEquals(JsonObject("i" to JsonNumber(1.0)), parseJson("""{"i":1}"""))
+        assertEquals(JsonObject("i" to JsonNumber(10.0)), parseJson("""{"i":10}"""))
+        assertEquals(JsonObject("i" to JsonNumber(3.14)), parseJson("""{"i":3.14}"""))
+    }
+
+    @Test
+    fun `should stringify boolean`() {
+        assertEquals(JsonObject("a" to JsonBoolean(true)), parseJson("""{"a":true}"""))
+        assertEquals(JsonObject("a" to JsonBoolean(false)), parseJson("""{"a":false}"""))
+    }
+
+    @Test
+    fun `should stringify string`() {
+        assertEquals(JsonObject("str" to JsonString("Some text")), parseJson("""{"str":"Some text"}"""))
+    }
+
+    @Test
+    fun `should stringify empty object inside object`() {
+        assertEquals(JsonObject("obj" to JsonObject(mapOf())), parseJson("""{"obj":{}}"""))
+    }
+
+    @Test
+    fun `should stringify empty object inside object inside object`() {
+        assertEquals(
+            JsonObject("obj" to JsonObject("obj2" to JsonObject())),
+            parseJson("""{"obj":{"obj2":{}}}""")
+        )
+    }
+
+    @Test
+    fun `should stringify all primitive types and ignore white spaces`() {
+        assertEquals(
+            JsonObject(
+                "a" to JsonString("AAA"),
+                "b" to JsonNumber(123.45),
+                "c" to JsonBoolean(true),
+                "d" to JsonBoolean(false),
+                "e" to JsonNull,
+            ),
+            parseJson("""{"a":"AAA","b":123.45,"c":true,"d":false,"e":null}""")
+        )
+    }
+
+    @Test
+    fun `should stringify all primitive types in object inside object`() {
+        assertEquals(
+            JsonObject(
+                "o" to JsonObject(
+                    "a" to JsonString("AAA"),
+                    "b" to JsonNumber(123.45),
+                    "c" to JsonBoolean(true),
+                    "d" to JsonBoolean(false),
+                    "e" to JsonNull,
+                )
+            ),
+            parseJson("""{"o":{"a":"AAA","b":123.45,"c":true,"d":false,"e":null}}""")
+        )
+    }
+
+    @Test
+    fun `should stringify array with strings`() {
+        assertEquals(
+            JsonObject(
+                "letters" to JsonArray(
+                    JsonString("A"),
+                    JsonString("B"),
+                    JsonString("C"),
+                )
+            ),
+            parseJson("""{"o":{"a":"AAA","b":123.45,"c":true,"d":false,"e":null}}""")
+        )
+    }
+
+    @Test
+    fun `should stringify array with numbers`() {
+        assertEquals(
+            JsonObject(
+                "numbers" to JsonArray(
+                    JsonNumber(1.0),
+                    JsonNumber(2.0),
+                    JsonNumber(3.0),
+                )
+            ),
+            parseJson("""{"numbers":[1,2,3]}""")
+        )
+    }
+
+    @Test
+    fun `should stringify array with objects`() {
+        assertEquals(
+            JsonObject(
+                "users" to JsonArray(
+                    JsonObject("name" to JsonString("Marcin")),
+                    JsonObject("name" to JsonString("Maja")),
+                )
+            ),
+            parseJson("""{"users":[{"name":"Marcin"},{"name":"Maja"}]}""")
+        )
+    }
+
+    @Test
+    fun `should parse stringified object`() {
+        val jsonElements = listOf(
+            JsonNumber(1.23),
+            JsonObject("name" to JsonString("This is some string")),
+            JsonArray(
+                JsonObject("name" to JsonString("Marcin")),
+                JsonObject("name" to JsonString("Maja")),
+            ),
+            JsonObject(
+                "a" to JsonArray(
+                    JsonObject("name" to JsonString("Marcin")),
+                    JsonObject("name" to JsonString("Maja")),
+                )
+            )
+        )
+        for (jsonElement in jsonElements) {
+            assertEquals(jsonElement, parseJson(jsonElement.stringify()))
+        }
+    }
+
+    @Test
+    fun `should return null for malformed jsons`() {
+        assertEquals(null, parseJson("{name:\"AAA\"}"))
+        assertEquals(null, parseJson("{\"name\":AAA}"))
+        assertEquals(null, parseJson("{\"name\":\"AAA\""))
+        assertEquals(null, parseJson("{\"name\"\"AAA\"}"))
+    }
+}
